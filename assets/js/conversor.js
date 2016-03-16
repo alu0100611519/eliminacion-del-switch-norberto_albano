b2362eec811862a8809f8dbd89e0844a6bd8584b
Albano Yanes <ajyanreyu@gmail.com>
2016-03-16 16:43:48 +0000

    Eliminacion de elementos innecesarios de conversor.js que se han pasado a medida.js

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -1,6 +1,3 @@
-(function(exports) {
-  "use strict";
-
   function Temperatura(valor,tipo)
   {
     /* tipo es opcional. Debería admitir new Medida("45.2 F") */
@@ -42,58 +39,16 @@
   //definimos la herencia de las clases
   Temperatura.prototype = new Medida();
   Temperatura.prototype.contructor = Temperatura;
+
   Celsius.prototype = new Temperatura();
   Celsius.prototype.contructor = Celsius;
+
   Farenheit.prototype = new Temperatura();
   Farenheit.prototype.constructor = Farenheit;
+  
   Kelvin.prototype = new Temperatura();
   Kelvin.prototype.constructor = Kelvin;
 
   exports.Temperatura = Temperatura;
   exports.Celsius = Celsius;
   exports.Farenheit = Farenheit;
-
-  exports.convertir = function() {
-    var valor     = document.getElementById('convert').value,
-        elemento  = document.getElementById('converted'),
-        /* Extienda la RegeExp a la especificación. use una XRegExp */
-        regexp    = XRegExp('(?<val> [-+]?\\d+(?:\.\\d+)?(?:e[+-]?\\d+)?\\s*) -?   #val  \n' +
-                            '(?<tipo1> ([cCfFkK])\\s* ) -?    #tipo1     \n' +
-                            '(?<tipo2> ([cCfFkK])\\s* ) -?    #tipo2     \n', 'x');
-                            //'(?<tipo2>  (([a-z,A-Z]+)\s*)) -?   #tipo2   \n');     /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([a-z,A-Z]+)\s*$/i
-    var val     = XRegExp.exec(valor , regexp );
-    console.log(val.val);
-    console.log(val.tipo1);
-    console.log(val.tipo2);
-    if (valor) {
-      var numero = val.val,
-          tipo1  = val.tipo1;
-     var  tipo2  = val.tipo2;
-
-
-      numero = parseFloat(numero);
-      console.log("Valor: " + numero + ", Tipo: " + tipo1 , "Tipo2: " + tipo2);
-
-      switch (tipo1) {
-        case 'c':
-          var celsius = new Celsius(numero);
-          elemento.innerHTML = celsius.toFarenheit().toFixed(2) + " Farenheit";
-          break;
-        case 'f':
-          var farenheit = new Farenheit(numero);
-          elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " Celsius";
-          break;
-        case 'k':
-          var kelvin = new Kelvin(numero);
-          elemento.innerHTML = kelvin.toCelsius().toFixed(2) + " Celsius";
-          break;
-
-        default:
-        converted.innerHTML = "ERROR! Intenta usar expresiones tipo '-2.7C' o '25.8K'"
-          /* rellene este código */
-      }
-    }
-    else
-      elemento.innerHTML = "";
-  };
-})(this);
