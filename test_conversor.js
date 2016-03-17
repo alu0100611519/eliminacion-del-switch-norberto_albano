234037070616e80adf57ce5eae88b4d67921e0ba
Albano Yanes <ajyanreyu@gmail.com>
2016-03-17 23:20:52 +0000

    pruebas para celcius

--- test_conversor.js
+++ test_conversor.js
@@ -14,3 +14,22 @@ describe("Clase Temperatura", function(){
     });
   });
 });
+
+describe("Clase Celcius", function(){
+  describe("constructor", function(){
+    it("Deberia existir un constructor", function(){
+      var celc = new Celsius (15e-25);
+      expect(celc.valor).to.equal(15e-25);
+    });
+    describe("Comprobamos las funciones", function(){
+      it("Conversion a farenheit", function(){
+        var celc = new Celcius(552);
+        expect(celc.toFarenheit()).to.equal(1025,6);
+      });
+      it("Conversion a Kelvin", function(){
+        var celc = new Celcius(0);
+        expect(celc.toKelvin()).to.equal(273,15);
+      });
+    });
+  });
+});
